ed46015df8b561882823a1ede1ee7dbe4ca43b44
M-Uzair-dev <uzairmanan3@gmail.com>
2023-09-01 00:09:03 +0500

    added contact page

--- src/Components/Contactpage.js
+++ src/Components/Contactpage.js
@@ -1,11 +1,44 @@
 import React from "react";
 import "./contact.css";
+import cardbgimg from "./images/contactbg.jpg";
+import agentimg from "./images/potrait.jpg";
 
 const contact = () => {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className="contactpage">
+      <div className="conleftside">
+        <div
+          className="coninerleft"
+          style={{
+            backgroundImage: `url(${cardbgimg})`,
+            backgroundSize: "cover",
+          }}
+        >
+          <div className="conlefttiltdiv">
+            <div className="agentpic">
+              <img src={agentimg} alt="Agent's Image" />
+            </div>
+            <p className="agentname">Dianne Russell</p>
+            <p className="agentdesc">
+              More than 20 years of experience in the field Real estate and has
+              worked with clients up to 100+
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="contactright">
+        <h1>Meet and talk with our best real estate agent</h1>
+        <p>
+          Our experienced real estate agent will guide you through the entire
+          home buying, selling, or renting process, regardless of your level of
+          knowledge. We will work with you to understand your needs and goals,
+          and we will provide you with the expert advice and support you need to
+          achieve your real estate goals.
+        </p>
+        <div className="contbuttondiv">
+          <button className="shine7">Contact</button>
+        </div>
+      </div>
     </div>
   );
 };
